72668e4bbe2db70a796207a56bb1fa3f57e47698
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-07-21 09:28:47 +0100

    refator - paddle

--- ball.py
+++ ball.py
@@ -1,17 +1,25 @@
+import pygame
 import game_constants
 
 class Ball:
-    def __init__(self, x: float, y: float, radius: float, ):
+    def __init__(self, x: float, y: float, radius: float = game_constants.BALL_RADIUS):
         self.x = self.original_x = x
         self.y = self.original_y = y
         self.radius = radius
+        self.max_vel = game_constants.BALL_MAX_VEL
         self.x_vel = game_constants.BALL_MAX_VEL
         self.y_vel = 0
         self.color = game_constants.BALL_COLOR
 
-    # TO DO
-    def draw(self, window):
-        pass
+    def __repr__(self):
+        return 'Ball(x: {}, y: {}, radius: {}, x_vel: {}, y_vel: {}, color: {})'.format(self.x,\
+            self.y, self.radius, self.x_vel, self.y_vel, self.color)
+
+    def __str__(self):
+        return self.__repr__()
+
+    def draw(self, window: pygame.Surface):
+        pygame.draw.circle(window, self.color, (self.x, self.y), self.radius)
 
     def move(self):
         self.x += self.x_vel
