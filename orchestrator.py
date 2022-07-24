df1881ade9ea711da7a4cdc298891e106796a741
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-07-24 16:14:20 +0100

    refator -Update orchestrator.py

--- orchestrator.py
+++ orchestrator.py
@@ -5,12 +5,14 @@ author: tiago vieira, tiagomvmvieira@hotmail.com
 
 #imports
 import pygame
-import game_constants
+import os
 
-from utils import *
+import extra_files.game_constants as game_constants
+
+from utils.util_functions import *
 from game_engine import *
-from ball import Ball
-from paddle import Paddle
+from cls.ball import Ball
+from cls.paddle import Paddle
 
 
 def main():
@@ -18,35 +20,35 @@ def main():
     window = pygame.display.set_mode((game_constants.WINDOW_WIDTH, game_constants.WINDOW_HEIGHT))
     pygame.display.set_caption("Pong Game")
 
+    game = True
     intro = True
     clock = pygame.time.Clock()
+    cwd = os.getcwd()
+    font_path = os.path.join(cwd, 'assets')
 
     while intro:
         intro_session(clock, game_constants.FPS, window, (game_constants.BLACK, game_constants.WHITE),
-        (game_constants.WINDOW_WIDTH, game_constants.WINDOW_HEIGHT), game_constants.WELCOME_FONT_SIZE)
+        (game_constants.WINDOW_WIDTH, game_constants.WINDOW_HEIGHT), (font_path, game_constants.WELCOME_FONT_SIZE),
+        [game_constants.COLOR_DIRECTION, game_constants.DEFINED_COLOR, game_constants.COLOR_VEL]
+        )
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 intro = False
+                game = False
                 break
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_KP_ENTER:
                     intro = False
                     break
 
-    run = True
-
     left_paddle = Paddle(10, game_constants.WINDOW_HEIGHT // 2 - game_constants.PADDLE_HEIGHT // 2)
     right_paddle = Paddle(game_constants.WINDOW_WIDTH - 10 - game_constants.PADDLE_WIDTH,\
         game_constants.WINDOW_HEIGHT // 2 - game_constants.PADDLE_HEIGHT // 2)
     ball = Ball(game_constants.WINDOW_WIDTH // 2, game_constants.WINDOW_HEIGHT // 2)
 
-    left_score = 0
-    right_score = 0
-    scores = [left_score, right_score]
-
-    game = True
-    run = True
+    
+    play = True
     victory = False
 
     while game:
@@ -54,29 +56,30 @@ def main():
         right_score = 0
         scores = [left_score, right_score]
 
-        while run:
+        while play:
             victory_text = ''
             keys = pygame.key.get_pressed()
             game_session(clock, game_constants.FPS, window, ball, (left_paddle, right_paddle), scores,
                         (game_constants.BLACK, game_constants.WHITE), (game_constants.WINDOW_WIDTH, game_constants.WINDOW_HEIGHT),
-                        game_constants.SCORE_FONT_SIZE, keys
+                        (font_path, game_constants.SCORE_FONT_SIZE), keys
                         )
 
             scores = score_handling(ball, scores, (left_paddle, right_paddle), game_constants.WINDOW_WIDTH)
 
             victory, victory_text = winner_handling(scores, victory, victory_text, game_constants.WINNING_SCORE)  
             if victory:
-                run = False
+                play = False
                 break
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    run = False
+                    play = False
                     break
         
         while victory:
             closing_session(window, victory_text, (game_constants.BLACK, game_constants.WHITE),
-                           (game_constants.WINDOW_WIDTH, game_constants.WINDOW_HEIGHT), game_constants.SCORE_FONT_SIZE
+                           (game_constants.WINDOW_WIDTH, game_constants.WINDOW_HEIGHT), (font_path, game_constants.WELCOME_FONT_SIZE),
+                           [game_constants.COLOR_DIRECTION, game_constants.DEFINED_COLOR, game_constants.COLOR_VEL]
                            )
 
             for event in pygame.event.get():
@@ -87,7 +90,7 @@ def main():
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_y:
                         victory = False
-                        run = True 
+                        play = True 
                         break
                     elif event.key == pygame.K_n:
                         victory = False
