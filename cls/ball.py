ca4e7894fb2226e8f392e421cad00aa426f7fdac
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-08-09 23:06:38 +0100

    Update ball cls

--- cls/ball.py
+++ cls/ball.py
@@ -1,14 +1,18 @@
 import pygame
+import math
+import random
 import extra_files.game_constants as game_constants
 
 class Ball():
     def __init__(self, x: float, y: float, radius: float = game_constants.BALL_RADIUS):
         self.x = self.original_x = x
         self.y = self.original_y = y
+        self.angle = self._get_random_angle(-30, 30, [0])
+        self.pos = 1 if random.random() < 0.5 else -1
         self.radius = radius
         self.max_vel = game_constants.BALL_MAX_VEL
-        self.x_vel = game_constants.BALL_MAX_VEL
-        self.y_vel = 0
+        self.x_vel = self.pos * abs(math.cos(self.angle) * self.max_vel)
+        self.y_vel = math.sin(self.angle) * self.max_vel
         self.color = game_constants.BALL_COLOR
 
     def __repr__(self):
@@ -17,6 +21,13 @@ class Ball():
 
     def __str__(self):
         return self.__repr__()
+
+    def _get_random_angle(self, min_angle: int, max_angle: int, excluded: list):
+        angle = 0
+        while angle in excluded:
+            angle = math.radians(random.randrange(min_angle, max_angle))
+
+        return angle
 
     def draw(self, window: pygame.Surface):
         pygame.draw.circle(window, self.color, (self.x, self.y), self.radius)
@@ -28,6 +39,11 @@ class Ball():
     def reset(self):
         self.x = self.original_x
         self.y = self.original_y
-        self.y_vel = 0
+
+        self.angle = self._get_random_angle(-30, 30, [0])
+        x_vel = abs(math.cos(self.angle) * self.max_vel)
+        y_vel = math.sin(self.angle) * self.max_vel
+
+        self.y_vel = y_vel
         self.x_vel *= -1
 
