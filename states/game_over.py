86c14b0c2fa63eb97cde23ceed00b26726d1a330
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-12-27 22:32:40 +0000

    Refactor GameOver state class to include _generate_time_active_message method and remove abstractmethod docs strings

--- states/game_over.py
+++ states/game_over.py
@@ -3,9 +3,12 @@ import os
 import extra_files.game_constants as game_constants
 
 from .base import BaseState
+from typing import Final
 
 
 class GameOver(BaseState):
+    _time_active_threshold: int = 10000
+
     def __init__(self)-> None:
         """__init__ constructor"""
         super().__init__()
@@ -23,15 +26,23 @@ class GameOver(BaseState):
                                         (self.window_height * 3 // 4 - self.revenge_message.get_height() // 2)
                                         )
         
-        self.time_active: int = 0
+        self.time_active: Final[int] = 0
 
-    def update(self, dt: int):
-        self.time_active += dt
-        if self.time_active >= 10000:
+    def _generate_time_active_message(self)-> None:
+        """This method generates the time active message block message to be draw"""
+        self.time_active_message = self.close_font.render('{}'.format(10 - (self.time_active // 1000)), True, game_constants.WHITE)
+        self.text_rect_active = self.time_active_message.get_rect()
+        self.text_rect_active.center = (self.window_width // 2 - self.time_active_message.get_width() // 2,
+                                    (self.window_height * 6 // 10 - self.time_active_message.get_height() // 2)
+                                    )
+
+    def update(self, dt: int)-> None:
+        self.time_active += dt #dt in miliseconds
+        self._generate_time_active_message()
+        if self.time_active >= self._time_active_threshold:
             self.quit = True
 
     def get_event(self, event: pygame.event.Event)-> None:
-        """This method handles how to react to specific events"""
         if event.type == pygame.QUIT:
             self.quit = True
         elif event.type == pygame.KEYDOWN:
@@ -42,7 +53,7 @@ class GameOver(BaseState):
                 self.quit = True
 
     def draw(self, surface: pygame.Surface)-> None:
-        """This method draws the specified components on the surface"""
         surface.fill(game_constants.PURE_BLACK)
         surface.blit(self.winner_message, self.text_rect_winner.center)
         surface.blit(self.revenge_message, self.text_rect_revenge.center)
+        surface.blit(self.time_active_message, self.text_rect_active.center)
