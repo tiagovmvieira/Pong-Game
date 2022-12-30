b987b61bbb86a012f15a8f08a2e5d633affb2b86
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-12-30 17:49:43 +0000

    Refactor Menu State class

--- states/menu.py
+++ states/menu.py
@@ -18,17 +18,17 @@ class Menu(BaseState):
         color = pygame.Color("red") if index == self.active_index else pygame.Color("white")
         return self.menu_font.render(self.options[index], True, color)
 
-    def get_text_position(self, text, index):
+    def get_text_position(self, text, index)-> None:
         center = (self.screen_rect.center[0], self.screen_rect.center[1] + (index * 50))
         return text.get_rect(center=center)
 
-    def handle_action(self):
+    def handle_action(self)-> None:
         if self.active_index == 0:
             self.done = True
         elif self.active_index == 1:
             self.quit = True
 
-    def get_event(self, event):
+    def get_event(self, event: pygame.event.Event)-> None:
         if event.type == pygame.QUIT:
             self.quit = True
         elif event.type == pygame.KEYUP:
@@ -49,6 +49,3 @@ class Menu(BaseState):
         for index, option in enumerate(self.options):
             text_render = self.render_text(index)
             surface.blit(text_render, self.get_text_position(text_render, index))
-
-    def startup(self, persistent)-> None:
-        self.persist = persistent
