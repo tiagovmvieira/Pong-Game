beb83aa7c5bf5d442186c1e43407c075f7962410
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-01-03 22:36:02 +0000

    Refactor Menu State class to asset private method nomenclature and update doc string on _render_text and _get_text_position methods

--- states/menu.py
+++ states/menu.py
@@ -14,15 +14,18 @@ class Menu(BaseState):
         self.options: list = ["Start Game", "Quit Game"]
         self.next_state: str = "GAMEPLAY"
 
-    def render_text(self, index)-> None:
+    def _render_text(self, index: int)-> pygame.Surface:
+        """This method renders the text grabbed through the index on the options attribute and returns the Pygame Surface"""
         color = pygame.Color("red") if index == self.active_index else pygame.Color("white")
         return self.menu_font.render(self.options[index], True, color)
 
-    def get_text_position(self, text, index)-> None:
+    def _get_text_position(self, text: pygame.Surface, index: int)-> tuple:
+        """This method computes and returns the text center position"""
         center = (self.screen_rect.center[0], self.screen_rect.center[1] + (index * 50))
         return text.get_rect(center=center)
 
-    def handle_action(self)-> None:
+    def _handle_action(self)-> None:
+        """This method handles the action concerning state flip based on the active index"""
         if self.active_index == 0:
             self.done = True
         elif self.active_index == 1:
@@ -37,7 +40,7 @@ class Menu(BaseState):
             elif event.key == pygame.K_DOWN:
                 self.active_index = 0 if self.active_index >= 1 else 1
             elif event.key == pygame.K_RETURN:
-                self.handle_action()
+                self._handle_action()
 
     def update(self, dt: int)-> None:
         """This method handles the update of the state"""
@@ -47,5 +50,5 @@ class Menu(BaseState):
         """This method draws the specified components on the surface"""
         surface.fill(game_constants.PURE_BLACK)
         for index, option in enumerate(self.options):
-            text_render = self.render_text(index)
-            surface.blit(text_render, self.get_text_position(text_render, index))
+            text_render = self._render_text(index)
+            surface.blit(text_render, self._get_text_position(text_render, index))
