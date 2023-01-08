e10e500e1fa6b2951e6f9fba5f0666a63436117a
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-01-08 11:39:43 +0000

    Refactor Ball class to fix methods' docstring and private nomenclature

--- cls/ball.py
+++ cls/ball.py
@@ -7,10 +7,11 @@ from cls.paddle import Paddle
 from typing import Optional
 
 class Ball():
-    def __init__(self, x: float, y: float, radius: float = game_constants.BALL_RADIUS):
+    def __init__(self, x: float, y: float, radius: float = game_constants.BALL_RADIUS)-> None:
+        """__init__ constructor"""
         self.x = self.original_x = x
         self.y = self.original_y = y
-        self.angle = self.__get_random_angle(-30, 30, [0])
+        self.angle = self._get_random_angle(-30, 30, [0])
         self.pos = 1 if random.random() < 0.5 else -1
         self.radius = radius
         self.max_vel = game_constants.BALL_MAX_VEL
@@ -18,59 +19,67 @@ class Ball():
         self.y_vel = math.sin(self.angle) * self.max_vel
         self.color = game_constants.BALL_COLOR
 
-    def __repr__(self):
+    def __repr__(self)-> str:
+        """__repr__ constructor"""
         return f"Ball(x: {self.x}, y: {self.y}, radius: {self.radius}, x_vel: {self.x_vel}, y_vel: {self.y_vel}, color: {self.color})"
         
-    def __str__(self):
+    def __str__(self)-> None:
+        """__str__ constructor"""
         return self.__repr__()
 
-    def __get_random_angle(self, min_angle: int, max_angle: int, excluded: list):
+    def _get_random_angle(self, min_angle: int, max_angle: int, excluded: list)-> float:
+        """This method returns the random angle used on the kick off stage"""
         angle = 0
         while angle in excluded:
             angle = math.radians(random.randrange(min_angle, max_angle))
 
         return angle
 
-    def __set_revertion_vel(self, x_direction: Optional[bool] = False, y_direction: Optional[bool] = False):
+    def _set_revertion_vel(self, x_direction: Optional[bool] = False, y_direction: Optional[bool] = False)-> None:
+        """This method reverts the signal of the ball along the specified direction (x_direction | y_direction)"""
         if x_direction:
             self.x_vel *= -1
         elif y_direction:
-            self.y_vel *= -1 
+            self.y_vel *= -1
 
-    def __set_y_vel_after_paddle_collision(self, paddle: Paddle):
+    def _set_y_vel_after_paddle_collision(self, paddle: Paddle)-> None:
+        """This method computes (based on the paddle's hitted position) and reverts the signal of the ball along the y_direction"""
         middle_y = paddle.y + paddle.height / 2
         difference_y = middle_y - self.y
         self.y_vel = -1 * difference_y / (paddle.height / 2) * self.max_vel
 
-    def draw(self, window: pygame.Surface):
+    def draw(self, window: pygame.Surface)-> None:
+        """This method draws the ball on the pygame.surface"""
         pygame.draw.circle(window, self.color, (self.x, self.y), self.radius)
 
-    def move(self):
+    def move(self)-> None:
+        """This method handles the movement of the ball updating it's position along the time"""
         self.x += self.x_vel
         self.y += self.y_vel
 
     def change_direction(self, paddle: Optional[Paddle] = None, up_boundary_collision: Optional[bool] = False,
                         down_boundary_collision: Optional[bool] = False, left_paddle_collision: Optional[bool] = False,
-                        right_paddle_collision: Optional[bool] = False):
-
+                        right_paddle_collision: Optional[bool] = False)-> None:
+        """This method handles the ball's change of direction behaviour"""
         if up_boundary_collision or down_boundary_collision:
-            self.__set_revertion_vel(y_direction= True)
+            self._set_revertion_vel(y_direction= True)
         elif left_paddle_collision:
             if self.y >= paddle.y and self.y <= paddle.y + paddle.height:
                 if self.x - self.radius <= paddle.x + paddle.width:
-                    self.__set_revertion_vel(x_direction = True)
-                    self.__set_y_vel_after_paddle_collision(paddle)
+                    self._set_revertion_vel(x_direction = True)
+                    self._set_y_vel_after_paddle_collision(paddle)
         elif right_paddle_collision:
             if self.y >= paddle.y and self.y <= paddle.y + paddle.height:
                 if self.x + self.radius >= paddle.x:
-                    self.__set_revertion_vel(x_direction = True)
-                    self.__set_y_vel_after_paddle_collision(paddle)
+                    self._set_revertion_vel(x_direction = True)
+                    self._set_y_vel_after_paddle_collision(paddle)
 
-    def reset(self):
+    def reset(self)-> None:
+        """This method resets the ball object by mutating some of the instance attribute variables"""
         self.x = self.original_x
         self.y = self.original_y
 
-        self.angle = self.__get_random_angle(-30, 30, [0])
+        self.angle = self._get_random_angle(-30, 30, [0])
         x_vel = abs(math.cos(self.angle) * self.max_vel)
         y_vel = math.sin(self.angle) * self.max_vel
 
