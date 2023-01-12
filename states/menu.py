21702326d7c868f1bbf7d73a7bf9f36faf2cb7ab
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-01-12 22:37:33 +0000

    Refactor Menu State class to include buttons logic

--- states/menu.py
+++ states/menu.py
@@ -2,17 +2,30 @@ import pygame
 import os
 
 from .base import BaseState
+from cls.button import Button
 import extra_files.game_constants as game_constants
 
 class Menu(BaseState):
+    pygame.init()
+    start_game_button: None = None
+    quit_game_button: None = None
+    menu_font: pygame.font = pygame.font.Font(os.path.join(BaseState.assets_dir, os.listdir(BaseState.assets_dir)[0]), 20)
+    
     def __init__(self)-> None:
         """__init__ constructor"""
         super().__init__()
-        self.menu_font = pygame.font.Font(os.path.join(self.assets_dir, os.listdir(self.assets_dir)[0]), 20)
 
-        self.active_index: int = 0
-        self.options: list = ["Start Game", "Quit Game"]
         self.next_state: str = "GAMEPLAY"
+
+    @classmethod
+    def set_menu_elements(cls, start_game_button: Button, quit_game_button: Button)-> None:
+        """This class method allocates on the start_game_button, quit_game_button cls variables the corresponding Menu objects"""
+        cls.start_game_button = start_game_button
+        cls.quit_game_button = quit_game_button
+
+    @classmethod
+    def get_menu_font(cls)-> pygame.font:
+        return cls.menu_font
 
     def _render_text(self, index: int)-> pygame.Surface:
         """This method renders the text grabbed through the index on the options attribute and returns the Pygame Surface"""
@@ -24,23 +37,20 @@ class Menu(BaseState):
         center = (self.screen_rect.center[0], self.screen_rect.center[1] + (index * 50))
         return text.get_rect(center=center)
 
-    def _handle_action(self)-> None:
+    def _handle_action(self, start_game: bool = False)-> None:
         """This method handles the action concerning state flip based on the active index"""
-        if self.active_index == 0:
+        if start_game:
             self.done = True
-        elif self.active_index == 1:
+        else:
             self.quit = True
 
     def get_event(self, event: pygame.event.Event)-> None:
         if event.type == pygame.QUIT:
             self.quit = True
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                self.active_index = 1 if self.active_index <= 0 else 0
-            elif event.key == pygame.K_DOWN:
-                self.active_index = 0 if self.active_index >= 1 else 1
-            elif event.key == pygame.K_RETURN:
-                self._handle_action()
+        elif self.start_game_button.check_click():
+            self._handle_action(start_game=True)
+        elif self.quit_game_button.check_click():
+            self._handle_action()
 
     def update(self, dt: int)-> None:
         """This method handles the update of the state"""
@@ -49,6 +59,6 @@ class Menu(BaseState):
     def draw(self, surface: pygame.Surface)-> None:
         """This method draws the specified components on the surface"""
         surface.fill(game_constants.PURE_BLACK)
-        for index, option in enumerate(self.options):
-            text_render = self._render_text(index)
-            surface.blit(text_render, self._get_text_position(text_render, index))
+        if self.start_game_button and self.quit_game_button:
+            self.start_game_button.draw(surface)
+            self.quit_game_button.draw(surface)
