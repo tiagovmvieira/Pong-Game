fc3826debe37f6035b04156a9470f42df8eed840
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-01-17 12:45:49 +0000

    Refactor Game class to abstract _bootrap_state logic

--- cls/game.py
+++ cls/game.py
@@ -4,11 +4,14 @@ import extra_files.game_constants as game_constants
 from cls.paddle import Paddle
 from cls.ball import Ball
 from cls.button import Button
-from typing import Final
+from cls.launcher import Launcher
 
 from states.base import BaseState
 from states.menu import Menu
 from states.game_over import GameOver
+
+from typing import Final
+
 
 class GameInformation:
     _left_player_score: Final[int] = 0
@@ -32,6 +35,7 @@ class MenuElements:
 
 class GameOverElements:
     _state_font = GameOver.get_state_font()
+    launcher = Launcher(game_constants.WINDOW_WIDTH / 5, game_constants.WINDOW_HEIGHT - game_constants.LAUNCHER_HEIGHT, 3000)
     _play_again_button = Button("Play Again", game_constants.COVER_BUTTON_WIDTH, game_constants.COVER_BUTTON_HEIGHT, (250, 250), 6,
                             _state_font)
     _quit_game_button = Button("Quit Game", game_constants.COVER_BUTTON_WIDTH, game_constants.COVER_BUTTON_HEIGHT, (250, 350), 6,
@@ -64,19 +68,18 @@ class GameStatesHandler:
         """This method stores the previous state resume attribute variable of the game into an attribute variable"""
         self.previous_state_resume = previous_state_resume
 
-    def _bootstrap_gameplay_state(self)-> None:
-        """This method includes the gameplay state bootstrap logic"""
-        self.state.set_game_elements(GameElements._left_paddle, GameElements._right_paddle, GameElements._ball)
-        self.state.set_initial_positions()
-        self.state.set_game_initial_score(GameInformation._left_player_score, GameInformation._right_player_score)
-
-    def _bootstrap_menu_state(self)-> None:
-        """This method includes the menu state bootstrap logic"""
-        self.state.set_state_elements(MenuElements._start_game_button, MenuElements._quit_game_button)
-
-    def _bootstrap_game_over_state(self)-> None:
-        """This method includes the game over state bootstrap logic"""
-        self.state.set_state_elements(GameOverElements._play_again_button, GameOverElements._quit_game_button)
+    def _bootstrap_state(self, **kwargs)-> None:
+        """This method includes the state bootstrap logic according to the fed kwargs"""
+        if kwargs.get("gameplay", False):
+            self.state.set_state_elements(GameElements._left_paddle, GameElements._right_paddle, GameElements._ball)
+            self.state.set_initial_positions()
+            self.state.set_game_initial_score(GameInformation._left_player_score, GameInformation._right_player_score)
+        elif kwargs.get("menu", False):
+            self.state.set_state_elements(MenuElements._start_game_button, MenuElements._quit_game_button)
+        elif kwargs.get("game_over", False):
+            self.state.set_state_elements(GameOverElements._play_again_button, GameOverElements._quit_game_button,
+                                        GameOverElements.launcher)
+            self.state.launcher.set_start_time()
 
     def flip_state(self)-> None:
         """This function flips the state assumed on the game"""
@@ -93,11 +96,11 @@ class GameStatesHandler:
         if self.state_name == "GAMEPLAY":
             if self.previous_state in ["MENU", "GAME_PAUSE"] and not self.previous_state_resume:
                 pygame.time.wait(300) if self.previous_state == "GAME_PAUSE" else None
-                self._bootstrap_gameplay_state()
+                self._bootstrap_state(gameplay=True)
         elif self.state_name == "MENU":
-            self._bootstrap_menu_state()
+            self._bootstrap_state(menu=True)
         elif self.state_name == "GAME_OVER":
-            self._bootstrap_game_over_state()
+            self._bootstrap_state(game_over=True)
 
     def update(self, dt: int)-> None:
         """This function updates.."""
@@ -123,6 +126,8 @@ class GameStatesHandler:
                 self.state.handle_collision()
                 self.state.score_handling()
                 self.state.winner_handling()
+            elif self.state_name == "GAME_OVER":
+                self.state.launcher.loop(game_constants.WINDOW_WIDTH, game_constants.WINDOW_HEIGHT)
 
             self.update(dt)
             self.draw()
