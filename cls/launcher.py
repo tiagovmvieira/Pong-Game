94924eda58e0a52d20616b88c235ad8ce5d54652
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-01-18 21:52:51 +0000

    Refactor Launcher class to fix @classmethod set_start_time logic

--- cls/launcher.py
+++ cls/launcher.py
@@ -6,7 +6,7 @@ import extra_files.game_constants as game_constants
 
 from .firework import Firework
 from typing import Final, Union
-
+   
 class Launcher:
     _width: Final[int] = game_constants.LAUNCHER_WIDTH
     _height: Final[int] = game_constants.LAUNCHER_HEIGHT
@@ -21,9 +21,12 @@ class Launcher:
         self.fireworks = []
 
     @classmethod
-    def set_start_time(cls)-> None:
+    def set_start_time(cls, current_time: Union[bool, None] = None)-> None:
         """This method sets the _start_time cls variable according to the instant time"""
-        cls._start_time = time.time()
+        if current_time:
+            cls._start_time = current_time
+        else:
+            cls._start_time = time.time()
 
     def draw(self, surface: pygame.Surface)-> None:
         """This method draws the launcher on the pygame.surface"""
@@ -31,29 +34,26 @@ class Launcher:
         launcher = pygame.Rect((self.x , self.y), (self._width, self._height))
         pygame.draw.rect(surface, "#354B5E", launcher)
 
-        print(self.fireworks)
-
         for firework in self.fireworks:
             firework.draw(surface)
 
     def launch(self)-> None:
-        """This method launches"""
+        """This method "launches" a firework by creating one and storing them on a list"""
         # color = random.choice(colors) # don't have colors
         color = "#354B5E"
         explode_height = random.randrange(50, 400) # pass into constants
 
-        firework = Firework(self.x + (self._width / 2), self.y, -5, explode_height, color)
+        firework = Firework(self.x + (self._width / 2), self.y, 5, explode_height, color)
         self.fireworks.append(firework)
 
     def loop(self, max_width: int, max_height: int)-> None:
-        """This method loops"""
-
+        """This method creates a loop that is responsible for launching the firework and moving the firework"""
         current_time: float = time.time()
         time_elapsed: float = current_time - self._start_time
 
         if time_elapsed * 1000 >= float(self.launch_frequency):
+            self.set_start_time(current_time=current_time)
             self.launch()
-            self._start_time = current_time
 
         # move all of the fireworks
         fireworks_to_remove = []
