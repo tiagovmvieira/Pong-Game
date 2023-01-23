b6eb6dd4874909a6f3adde81de3b126983ac5ad5
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-01-23 21:28:08 +0000

    Refactor Launcher class to include firework's update logic inside update method

--- cls/launcher.py
+++ cls/launcher.py
@@ -1,13 +1,12 @@
 import pygame
 import time
 import random
-import threading
 
 import extra_files.game_constants as game_constants
 
 from .firework import Firework
-from typing import Final, Union
-   
+from typing import Final, Union, List
+
 class Launcher:
     _width: Final[int] = game_constants.LAUNCHER_WIDTH
     _height: Final[int] = game_constants.LAUNCHER_HEIGHT
@@ -19,7 +18,7 @@ class Launcher:
         self.launch_frequency = launch_frequency # ms
 
         self._start_time: Union[None, float] = None
-        self.fireworks = []
+        self.fireworks: List[Firework] = []
 
     def __repr__(self)-> str:
         """__repr__ constructor"""
@@ -28,6 +27,12 @@ class Launcher:
     def __str__(self)-> None:
         """__str__ constructor"""
         return self.__repr__()
+
+    def update(self, surface: pygame.Surface):
+        for firework in self.fireworks:
+            firework.update(surface)
+            if firework.remove():
+                self.fireworks.remove(firework)
 
     def reset_start_time(self, current_time: Union[float, None] = None)-> None:
         """This method resets the _start_time instance variable according to a time value"""
@@ -42,19 +47,12 @@ class Launcher:
         launcher = pygame.Rect((self.x , self.y), (self._width, self._height))
         pygame.draw.rect(surface, "#354B5E", launcher)
 
-        for firework in self.fireworks:
-            firework.draw(surface)
-
     def launch(self)-> None:
         """This method "launches" a firework by creating one and storing it on a list"""
-        # color = random.choice(colors) # don't have colors
-        color = "#354B5E"
-        explode_height = random.randrange(50, 400) # pass into constants
-
-        firework = Firework(self.x + (self._width / 2), self.y, 5, explode_height, color)
+        firework = Firework(self.x + (self._width / 2), self.y, random.randrange(50, 400))
         self.fireworks.append(firework)
 
-    def loop(self, max_width: int, max_height: int)-> None:
+    def loop(self, surface: pygame.Surface, max_width: int, max_height: int)-> None:
         """This method creates a loop that is responsible for launching the firework and moving the firework"""
         current_time: float = time.time()
         time_elapsed: float = current_time - self._start_time
@@ -63,12 +61,9 @@ class Launcher:
             self.reset_start_time(current_time=current_time)
             self.launch()
 
-        # move all of the fireworks
-        fireworks_to_remove = []
         for firework in self.fireworks:
-            firework.move(max_width, max_height)
-            if firework.exploded and len(firework.projectiles) == 0:
-                fireworks_to_remove.append(firework)
+            firework.update(surface, max_width, max_height)
+            if firework.remove():
+                self.fireworks.remove(firework)
 
-        for firework in fireworks_to_remove:
-            self.fireworks.remove(firework)
+        pygame.display.update()
